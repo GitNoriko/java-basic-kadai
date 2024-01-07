98831bc4e42d1476e204c75344c9c792a23df300
GitNoriko <151433763+GitNoriko@users.noreply.github.com>
2024-01-07 23:20:35 +0900

    swich文に変更

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -4,20 +4,20 @@ public class Price_Chapter08 {
 
     public static void main(String[] args) {
         int userAge = 30;
-        int serviceCost = 0;
-        if (userAge < 20) {
-            serviceCost = 1000;
-        } else if (userAge < 30) {
-            serviceCost = 2000;
-        } else if (userAge < 50) {
-            serviceCost = 3000;
-        } else if (userAge < 80) {
-            serviceCost = 4000;
-        } else if (userAge < 90) {
-            serviceCost = 5000;
-        } else {
-            serviceCost = 500;
-        }
-        System.out.println(userAge + "代の料金は" + serviceCost + "円");
+        int serviceCost;
+        
+        serviceCost = switch (userAge) {
+    	case 10 -> 1000;
+    	case 20 -> 2000;
+    	case 30 -> 3000;
+    	case 40 -> 3000;
+    	case 50 -> 4000;
+    	case 60 -> 4000;
+    	case 70 -> 4000;
+    	case 80 -> 5000;
+    	default -> 500;	
+    	};
+    	
+    	System.out.print(userAge + "代の料金は" + serviceCost + "円");	
     }
-}
+}        
